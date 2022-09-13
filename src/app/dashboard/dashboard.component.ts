f6e7304bcb9267ac959efed4a80d5e8d2c8f9c71
cpshibinoppo <cpshibinoppo@gmail.com>
2022-09-13 16:51:43 +0530

    size

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  add: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  show() {
+    var box = document.getElementById('hideprof');
+    this.add = true;
+    box!.style.display = 'block';
   }
-
+  hide() {
+    var box = document.getElementById('hideprof');
+    this.add = false;
+    box!.style.display = 'none';
+  }
 }
